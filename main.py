52c4ce85f0e6d1671c765a8dbd21504b43098cef
rushikesh gaikwad <lolits2626@gmail.com>
2026-04-19 00:00:55 +0530

    api route updated

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ app = FastAPI()
 def home():
     return "Everything is Good!.."
 
-@app.get("/products")
-def getProducts():
-    return products
+# @app.get("/products")
+# def getProducts():
+#     return products
 
 @app.get("/products/{id}")
 def getProductById(id:int):
     if id<1 or id>len(products):
         raise HTTPException(status_code=404,details="products not found")
     return products[id-1]
+
+@app.get("/products")
+def getProductByQuery(category:str=None):
+    if category!=None:
+        res = [n for n in products if n["category"]==category]
+        return res
+    else:
+        return products
